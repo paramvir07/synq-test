4f280f65622e8b8389663c50d8933ab0c8faa62f
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2025-12-25 18:03:25 -0800

    pls chl jaa

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -3,11 +3,11 @@ import { NextRequest } from 'next/server'
 import { ConvexHttpClient } from 'convex/browser'
 import { api } from '@/convex/_generated/api'
 
-const convex = new ConvexHttpClient(process.env.CONVEX_URL!)
 
 export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req)
+    const convex = new ConvexHttpClient(process.env.CONVEX_URL!)
     console.log(evt.data)
     if (evt.type === 'user.created' || evt.type === 'user.updated') {
       const data = evt.data

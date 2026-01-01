758341f1725638b254bef5ad05538f92942bb5f4
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2025-12-31 20:16:06 -0800

    fixed room creation

--- convex/room.ts
+++ convex/room.ts
@@ -2,17 +2,17 @@ import { v } from "convex/values";
 import { mutation, query, MutationCtx } from "./_generated/server";
 
 const generateRandomRoomCode = async (ctx: MutationCtx) => {
-  while (true) {
+  for (let i = 0; i < 10; i++) { // try 10 times max
     const randomRoomCode = Math.floor(100000 + Math.random() * 900000);
-
-    const existingRoom = ctx.db
+    const existingRoom = await ctx.db
       .query("room")
       .withIndex("byRoomCode", (q) => q.eq("roomCode", randomRoomCode))
       .unique();
-
     if (!existingRoom) return randomRoomCode;
   }
+  throw new Error("Could not generate unique 6-digit room code after 10 tries");
 };
+
 
 export const getRooms = query({
   args: {},
@@ -37,7 +37,7 @@ export const createRoom = mutation({
     }
 
     const roomcode = await generateRandomRoomCode(ctx);
-
+    
     await ctx.db.insert("room", {
       roomCode: roomcode,
       hostId: user._id,
